9229a93a969e9b14e903d56ff2f77a4d544793d4
Victor Fernandez <victor.fdz.11@gmail.com>
2020-04-27 18:06:32 -0500

    Scope

--- assets/index.js
+++ assets/index.js
@@ -1,32 +1,8 @@
+import MediaPlayer from './MediaPlayer.js'
+
 const video = document.querySelector("video")
 const button = document.querySelector("button")
-
-class MediaPlayer {
-    constructor(config) {
-        this.media = config.element
-    }
-    play() {
-        this.media.play()
-    }
-    pause() {
-        this.media.pause()
-    }
-    togglePlay() {
-        (this.media.paused)
-            ? this.play()
-            : this.pause()
-    }
-    // paused() {
-    //   return this.media.paused
-    // }
-}
 
 const player = new MediaPlayer({ element: video })
 
 button.onclick = () => player.togglePlay()
-
-      // button.onclick = () => {
-      //   (player.paused())
-      //     ?player.play()
-      //     :player.pause()
-      // } 
